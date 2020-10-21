f67ec50df4b1bb32cd0b1c87144f2aca1c5ea23f
Sara Kaoukabi <sara.kaoukabi@cognite.com>
2020-10-21 21:56:01 +0200

    Fixed gradle build

--- more_code/Vulnerable.java
+++
@@ -1,8 +0,0 @@
-//import SQLInjectionSample;
-
-public class Vulnerable extends SQLInjectionSample {
-
-    public static void main(String[] args) {
-        sample = new SQLInjectionSample();
-    }
-}
